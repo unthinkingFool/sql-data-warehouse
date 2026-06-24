9474c895171d64b3951b48ce1e1738aa646f0447
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-06-25 01:17:13 +0600

    Add dwh_create_time column to silver tables

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -9,7 +9,6 @@ Script Purpose:
 ===============================================================================
 */
 
-
 IF OBJECT_ID('silver.crm_cust_info','U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
 
@@ -20,7 +19,8 @@ CREATE TABLE silver.crm_cust_info(
 	cst_lastname NVARCHAR(50),
 	cst_marital_status NVARCHAR(50),
 	cst_gender NVARCHAR(50),
-	cst_create_date DATE
+	cst_create_date DATE,
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
@@ -33,7 +33,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_cost     INT,
     prd_line     NVARCHAR(50),
     prd_start_dt DATETIME,
-    prd_end_dt   DATETIME
+    prd_end_dt   DATETIME,
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.crm_sales_details','U') IS NOT NULL
@@ -48,7 +49,8 @@ CREATE TABLE silver.crm_sales_details (
     sls_due_dt   INT,
     sls_sales    INT,
     sls_quantity INT,
-    sls_price    INT
+    sls_price    INT,
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
@@ -56,7 +58,8 @@ IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
 
 CREATE TABLE silver.erp_loc_a101 (
     cid    NVARCHAR(50),
-    cntry  NVARCHAR(50)
+    cntry  NVARCHAR(50),
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.erp_cust_az12','U') IS NOT NULL
@@ -65,7 +68,8 @@ IF OBJECT_ID('silver.erp_cust_az12','U') IS NOT NULL
 CREATE TABLE silver.erp_cust_az12 (
     cid    NVARCHAR(50),
     bdate  DATE,
-    gen    NVARCHAR(50)
+    gen    NVARCHAR(50),
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.erp_px_cat_g1v2','U') IS NOT NULL
@@ -75,5 +79,6 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     id           NVARCHAR(50),
     cat          NVARCHAR(50),
     subcat       NVARCHAR(50),
-    maintenance  NVARCHAR(50)
+    maintenance  NVARCHAR(50),
+    dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
