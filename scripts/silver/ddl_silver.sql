bebd87c14f7d3e7bab4a5cb57947ba4ee37eb3d5
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-06-25 02:39:58 +0600

    Modify crm_prd_info table structure and data types

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -28,12 +28,13 @@ IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
 
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
+    cat_id       NVARCHAR(50),
     prd_key      NVARCHAR(50),
     prd_nm       NVARCHAR(50),
     prd_cost     INT,
     prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME,
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
     dwh_create_time DATETIME2 DEFAULT GETDATE()
 );
 
