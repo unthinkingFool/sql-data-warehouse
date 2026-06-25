472da6baa507162fea9094f18a92d56adda1616b
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-06-25 12:30:29 +0600

    Add insert statement for erp_px_cat_g1v2
    Insert data into silver.erp_px_cat_g1v2 from bronze.erp_px_cat_g1v2.

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -168,3 +168,22 @@ SELECT
 		 ELSE TRIM(cntry)
 	END cntry -- normalize and handle the missing or blank country codes
 FROM bronze.erp_loc_a101
+
+
+
+
+--====================================================
+--====================================================
+INSERT INTO silver.erp_px_cat_g1v2 
+(	
+	id,
+	cat,
+	subcat,
+	maintenance
+)
+SELECT
+	id,
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2
