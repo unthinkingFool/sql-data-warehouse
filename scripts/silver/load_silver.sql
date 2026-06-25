6d76563ef4c9292499bda50ac25d29b8f5568ad4
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-06-25 12:24:06 +0600

    Insert normalized country data into silver.erp_loc_a101
    Added SQL insert statement to normalize country codes.

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -152,3 +152,19 @@ SELECT  CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LEN(cid))
             ELSE 'n/a'
         END gen
   FROM bronze.erp_cust_az12
+
+
+
+
+
+--======================================================
+--======================================================
+INSERT INTO silver.erp_loc_a101 (cid,cntry)
+SELECT
+	REPLACE(cid,'-','') cid,
+	CASE WHEN TRIM(cntry)='DE' THEN 'Germany'
+		 WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+		 WHEN TRIM(cntry)='' OR cntry IS NULL THEN 'n/a'
+		 ELSE TRIM(cntry)
+	END cntry -- normalize and handle the missing or blank country codes
+FROM bronze.erp_loc_a101
