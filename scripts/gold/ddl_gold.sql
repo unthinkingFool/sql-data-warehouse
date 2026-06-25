6139e3ebc7c84fc90cd90c6947a8c3167b2e4cb2
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-06-25 15:48:39 +0600

    Create view for dimension products in gold schema

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -42,6 +42,22 @@ LEFT JOIN silver.erp_loc_a101 la ON la.cid=ci.cst_key
 -- DIMENSION PRODUCT
 --==========================================================
 
+CREATE VIEW gold.dim_products AS
+SELECT ROW_NUMBER() OVER(ORDER BY  pn.prd_start_dt,pn.prd_id) AS product_key
+      ,pn.prd_id AS product_id
+      ,pn.prd_key AS product_number
+      ,pn.prd_nm AS product_name
+      ,pn.cat_id AS category_id
+      ,pcg.cat AS category
+      ,pcg.subcat AS subcategory
+      ,pcg.maintenance 
+      ,pn.prd_cost AS cost
+      ,pn.prd_line AS product_line
+      ,pn.prd_start_dt AS start_date
+
+  FROM silver.crm_prd_info pn
+  LEFT JOIN silver.erp_px_cat_g1v2 pcg ON  pcg.id=pn.cat_id
+  WHERE pn.prd_end_dt IS NULL -- filter out historical data 
 
 
 
