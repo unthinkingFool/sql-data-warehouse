7d6e3d72b3d461c7481155b7cef0915fa0be04cd
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-06-25 12:09:22 +0600

    Add INSERT statement for customer data processing
    Insert customer data into silver.erp_cust_az12 with transformations.

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -127,5 +127,28 @@ SELECT
         END sls_price -- recalculating price data if there is any missing or mismatched value
       
   FROM bronze.crm_sales_details
- 
 
+
+
+
+
+--=====================================================
+--=====================================================
+ 
+INSERT INTO silver.erp_cust_az12
+(
+    cid,
+    bdate,
+    gen
+)
+SELECT  CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LEN(cid))
+             ELSE cid
+        END  cid
+      ,CASE WHEN bdate>GETDATE() THEN NULL
+            ELSE bdate
+       END bdate
+      ,CASE WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
+            WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
+            ELSE 'n/a'
+        END gen
+  FROM bronze.erp_cust_az12
