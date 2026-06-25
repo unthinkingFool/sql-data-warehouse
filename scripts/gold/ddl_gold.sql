f24ad449c2b850281285ad3b0172de385dc7549d
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-06-25 15:32:49 +0600

    Create Gold views for customers and sales
    This script creates views for the Gold layer in the data warehouse, including a view for customers with relevant transformations and data combinations from the Silver layer.

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,1 +1,53 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
 
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+--===============================================================
+-- DIMENTION CUSTOMERS
+--===============================================================
+CREATE VIEW gold.dim_customers AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	la.cntry AS country,
+	ci.cst_marital_status AS marital_status,
+	CASE WHEN ci.cst_gender!='n/a' THEN ci.cst_gender -- considering CRM as the master table
+		 ELSE COALESCE(ca.gen,'n/a') 
+	END AS gender,
+	ca.bdate AS birthdate,
+	ci.cst_create_date AS create_date
+
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca ON ca.cid=ci.cst_key 
+LEFT JOIN silver.erp_loc_a101 la ON la.cid=ci.cst_key
+
+
+
+--==========================================================
+-- DIMENSION PRODUCT
+--==========================================================
+
+
+
+
+
+
+
+--===========================================================
+-- FACT SALES
+--============================================================
